8180fcba699c167beaf380ba62952f68461bc4b2
Mark Johnson <xfoundry27@yahoo.ca>
2020-02-25 20:06:34 -0500

    more cleanly separate convenience quoting macros (mov) from underlying functions (0mov)

--- include/ppasm/keywords.h
+++ include/ppasm/keywords.h
@@ -1,23 +1,23 @@
+#define inc(reg) 0inc(8(reg))
 
-#define inc(reg) 8ppasm_insn_inc(8quote(reg))
+#define push(reg) 0push(8(reg))
 
-#define push(reg) 8ppasm_insn_push(8quote(reg))
+#define mov(a, b) 0mov(8(a), 8(b))
 
-#define mov(a, b) 8ppasm_insn_mov(8quote(a), 8quote(b))
+#define pop(reg) 0pop(8(reg))
 
-#define pop(reg) 8ppasm_insn_pop(8quote(reg))
-
-#define ret 8ppasm_insn_ret(8nil)
-
-
-#define dword(...) 8ppasm_indop_parse(8((__VA_ARGS__)))
+#define ret 0ret(8nil)
 
 
-#define label(l, insn) 8ppasm_intrinsic_label(8quote(l), insn)
+#define byte(...) ORDER_PP(0byte(8((__VA_ARGS__))))
 
-#define define(name, var) 8ppasm_intrinsic_define(8quote(name), var)
+#define word(...) ORDER_PP(0word(8((__VA_ARGS__))))
 
-#define section(name, ...) 8ppasm_intrinsic_section(8quote(name), 8quote((__VA_ARGS__)))
+#define dword(...) ORDER_PP(0dword(8((__VA_ARGS__))))
+
+#define qword(...) ORDER_PP(0qword(8((__VA_ARGS__))))
 
 
-#define macro_apply(insn, ...) 8eval(8env_nil, 8adjacent(8(insn), 8tuple(__VA_ARGS__)))
+#define label(l, insn) 0label(l, insn)
+
+#define section(name, ...) 0section(8(name), 8((__VA_ARGS__)))
--- include/ppasm/ppasm.h
+++ include/ppasm/ppasm.h
@@ -6,7 +6,7 @@
 #include "keywords.h"
 
 
-#define ORDER_PP_DEF_8ppasm_insn_inc \
+#define ORDER_PP_DEF_0inc \
   ORDER_PP_FN(8fn(8R, 8E,			\
   				8let((8S, 8cat(8(PPASM_REGISTER_SIZE_), 8R))(8I, 8cat(8(PPASM_REGISTER_INDEX_), 8R))(8B, 8cat(8(PPASM_REGISTER_BLOCK_), 8R)), \
 				  	8cond( \
@@ -16,56 +16,56 @@
 						  (8and(8equal(8S, 32), 8equal(8B, 1)), 8seq(8to_lit(8nat(6,5)), 8to_lit(8nat(2,5,5)), 8to_lit(8add(8nat(1,9,2), 8I)))) \
 						  (8else, 8exit(0))))))
 
-#define ORDER_PP_DEF_8ppasm_insn_push \
+#define ORDER_PP_DEF_0push \
   ORDER_PP_FN(8fn(8R, 8E,			\
 		  8seq(8to_lit(8nat(5, 5)))))
 
-#define ORDER_PP_DEF_8ppasm_insn_pop \
+#define ORDER_PP_DEF_0pop \
   ORDER_PP_FN(8fn(8R, 8E,			\
 		  8seq(8to_lit(8nat(1, 0)))))
 
-#define ORDER_PP_DEF_8ppasm_insn_mov \
+#define ORDER_PP_DEF_0mov \
   ORDER_PP_FN(8fn(8A, 8B, 8E,			\
-		  8seq(8to_lit(8nat(6, 5)))))
+		  8seq(8B)))
 
-#define ORDER_PP_DEF_8ppasm_insn_ret \
+#define ORDER_PP_DEF_0ret \
   ORDER_PP_FN(8fn(8X, 8E, 				\
-		  8ppasm_make_pse( \
+		  0ppasm_make_pse( \
 				  8seq(8to_lit(8nat(1, 9, 5))))))
 
 
 
 
 
-#define ORDER_PP_DEF_8ppasm_parse_state_nil ORDER_PP_CONST()
+#define ORDER_PP_DEF_0ppasm_parse_state_nil ORDER_PP_CONST()
 
-#define ORDER_PP_DEF_8ppasm_pse_nil() ORDER_PP_MACRO(8env_nil)
+#define ORDER_PP_DEF_0ppasm_pse_nil() ORDER_PP_MACRO(8env_nil)
 
-#define ORDER_PP_DEF_8ppasm_make_pse \
+#define ORDER_PP_DEF_0ppasm_make_pse \
   ORDER_PP_FN(8fn(8B, \
-		  8ppasm_pse_set(8(8B), 8ppasm_pse_nil(), 8B)))
-  
-#define ORDER_PP_DEF_8ppasm_pse_set		\
+		  0ppasm_pse_set(8(8B), 0ppasm_pse_nil(), 8B)))
+
+#define ORDER_PP_DEF_0ppasm_pse_set		\
   ORDER_PP_FN(8fn(8S, 8E, 8X, 8env_bind(8S, 8E, 8X)))
 
-#define ORDER_PP_DEF_8ppasm_pse_get              \
+#define ORDER_PP_DEF_0ppasm_pse_get              \
   ORDER_PP_FN(8fn(8S, 8E, 8env_ref(8S, 8E)))
 
 
 
 
-#define ORDER_PP_DEF_8ppasm_eval \
+#define ORDER_PP_DEF_0ppasm_eval \
   ORDER_PP_FN(8fn(8F, 8E, 8eval(8env_nil, 8F)))
 
-#define ORDER_PP_DEF_8ppasm_process_insn \
+#define ORDER_PP_DEF_0ppasm_process_insn \
   ORDER_PP_FN(8fn(8O, 8E, \
 		  8seq_join( \
 			    8E,						\
-			    8ap(8ppasm_eval(8O, 8E), 8E))))
+			    8ap(0ppasm_eval(8O, 8E), 8E))))
 
-#define ORDER_PP_DEF_8ppasm_expand_macro \
+#define ORDER_PP_DEF_0ppasm_expand_macro \
   ORDER_PP_FN(8fn(8M, 8E, \
-		  8let((8R, 8ppasm_eval(8M, 8E)), \
+		  8let((8R, 0ppasm_eval(8M, 8E)), \
 		       8seq_fold(8fn(8A, 8O, 8seq_join(8A, 8ap(8O, 8E))),	\
 				 8nil,					\
 				 8cond(\
@@ -73,25 +73,25 @@
 				       (8is_seq(8R), 8R) \
 				       (8else, 8seq(8R)))))))
 
-#define ORDER_PP_DEF_8ppasm_expand_macro_seq \
+#define ORDER_PP_DEF_0ppasm_expand_macro_seq \
   ORDER_PP_FN(8fn(8S, 8E, \
-                  8seq_fold(8fn(8A, 8O, 8ppasm_expand_macro(8O, 8A)), \
+                  8seq_fold(8fn(8A, 8O, 0ppasm_expand_macro(8O, 8A)), \
                             8E, \
                             8S)))
-  
-#define ORDER_PP_DEF_8ppasm_expand_macro_tup \
-  ORDER_PP_FN(8fn(8T, 8E, 8ppasm_expand_macro_seq(8tuple_to_seq(8T), 8E)))
 
-#define ORDER_PP_DEF_8ppasm_assemble \
+#define ORDER_PP_DEF_0ppasm_expand_macro_tup \
+  ORDER_PP_FN(8fn(8T, 8E, 0ppasm_expand_macro_seq(8tuple_to_seq(8T), 8E)))
+
+#define ORDER_PP_DEF_0ppasm_parse \
   ORDER_PP_FN(8fn(8S,\
 		  8seq_fold(8fn(8E, 8O, \
-				8cond((8is_tuple(8O), 8ppasm_expand_macro_tup(8O, 8E)) \
-				      (8else, 8ppasm_process_insn(8O, 8E)))), \
-			    8ppasm_parse_state_nil, \
+				8cond((8is_tuple(8O), 0ppasm_expand_macro_tup(8O, 8E)) \
+				      (8else, 0ppasm_process_insn(8O, 8E)))), \
+			    0ppasm_parse_state_nil, \
 			    8S)))
 
 
-#define ORDER_PP_DEF_8ppasm_emit_insn_bytes \
+#define ORDER_PP_DEF_0ppasm_emit_insn_bytes \
   ORDER_PP_FN(8fn(8S, \
 		  8stringize(						\
 			     8seq_fold(8fn(8A, 8B, \
@@ -101,6 +101,9 @@
 				       8S))))
 
 
-#define assemble(...) ORDER_PP(			\
-						      8ppasm_assemble(	\
-								      8tuple_to_seq(8quote((__VA_ARGS__)))))
+#define ORDER_PP_DEF_0assemble \
+  ORDER_PP_FN(8fn(8T, \
+  		  8let((8P, 0ppasm_parse(8tuple_to_seq(8T))), \
+				8P)))
+
+#define assemble(...) ORDER_PP(0assemble(8((__VA_ARGS__))))
